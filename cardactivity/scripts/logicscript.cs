34768faf120f0772b80a7c42ed69618e9c3c42c9
Muggingfrenchturtle <162884199+Muggingfrenchturtle@users.noreply.github.com>
2025-02-01 23:18:06 +0800

    background

--- cardactivity/scripts/logicscript.cs
+++ cardactivity/scripts/logicscript.cs
@@ -26,15 +26,22 @@ public partial class logicscript : Node2D
 		GD.Print("shaking");
         Tween tweener = GetTree().CreateTween();
 
-        
+        /*
         tweener.TweenCallback(Callable.From(shakerand));
         tweener.TweenCallback(Callable.From(shakerand));
         tweener.TweenCallback(Callable.From(shakerand));
         tweener.TweenCallback(Callable.From(shakerand));
         tweener.TweenCallback(Callable.From(shakerand));
-        //tweener.TweenCallback(Callable.From(shakeReset));
-        
-        //tweener.TweenProperty(this, "position", )
+        tweener.TweenCallback(Callable.From(shakeReset));
+        */
+        var shaketime = 0.3;
+        var intensity = 15;
+        tweener.TweenProperty(this, "position:x", GD.Randi() % intensity, shaketime);
+        tweener.TweenProperty(this, "position:y", GD.Randi() % intensity, shaketime);
+        tweener.TweenProperty(this, "position:x", GD.Randi() % intensity, shaketime);
+        tweener.TweenProperty(this, "position:y", GD.Randi() % intensity, shaketime);
+        tweener.TweenProperty(this, "position:x", GD.Randi() % intensity, shaketime);
+        tweener.TweenProperty(this, "position",Vector2.Zero, shaketime);
 
     }
 
